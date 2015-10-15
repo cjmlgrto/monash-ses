a9d9aa6db2bd6e3dd27f0af64da6fdfcd53ce207
CJ Melegrito <cj@mlgrto.com>
2015-10-15 11:52:40 +1100

    Added subheads
    aka a to-do list… there’s A LOT to do :(

--- v2.1/CourseAdmin.py
+++ v2.1/CourseAdmin.py
@@ -4,3 +4,57 @@ from Unit import Unit
 from Student import Student
 
 class CourseAdmin:
+	
+	# UNIT METHODS
+	
+	# - Display All Units
+	
+	# - Create Unit
+	
+	# - Search Unit
+	
+	# - Display Students in Unit
+	
+	# - Edit Unit Details
+	
+	# - Delete Unit
+	
+	
+	# STUDENT METHODS
+	
+	# - Create Student
+	
+	# - Search Student
+	
+	# - Enrol Student
+	
+	# - Display Undergraduate Students
+	
+	# - Display Postgraduate Students
+	
+	# - Display Domestic Students
+	
+	# - Display International Students
+	
+	# - Is Student enrolled in Unit?
+	
+	# - Is Student enrolled in Course?
+	
+	# - Edit Student Details
+	
+	# - Delete Student
+	
+	
+	# COURSE METHODS
+	
+	# - Create Course
+	
+	# - Search Course
+	
+	# - Display All Courses
+	
+	# - Display Units in Course
+	
+	# - Edit Course Details
+	
+	# - Delete Course
